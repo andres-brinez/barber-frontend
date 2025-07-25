c1de7db58565bf240d5ba50ba77130b677ac2906
Andres Briñez <brinezlopez08@gmail.com>
2025-07-24 22:17:24 -0500

    Update metadata title and language in layout;

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Barberia',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
         <style>{`
 html {
